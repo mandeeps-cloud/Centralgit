2806b4b48cc5696a74d5631ba9efdd695fbe940a
root <root@jenkins.us-central1-a.c.gcp-cloudzune.internal>
2021-06-18 04:25:32 +0000

    python scripts

---
+++ twinkle.py
@@ -0,0 +1,16 @@
+
+'''This is the solution of Practice test 1 question'''
+
+#when there are multiple lines in the string ,
+#then you need to use ''' to print it otherwise "" will exists.
+
+#Single Line Comment
+'''Multiple line Comment'''
+#If you want to use multiple lines words and statements then use [''' words '''].
+
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
+
