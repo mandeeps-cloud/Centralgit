9588e58a41877a82cb01ad04ab00367439750bfc
root <root@jenkins.us-central1-a.c.gcp-cloudzune.internal>
2021-06-18 04:41:13 +0000

    python begginer

---
+++ filesplit.py
@@ -0,0 +1,18 @@
+# Author: Mandeep Singh
+# Location: Moon
+# Date: 23/09/2023
+
+import os
+#import flask 
+from fsplit.filesplit import Filesplit
+fs = Filesplit()
+print(os.listdir()) 
+#Print the contents of the directory using os module.
+#os.listdir()) is a external module Print the contents of the directory using os module.
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,9 @@
+
+'''Author::Mandeep Singh
+   Chapter 1 Python
+******************Thanks for reading*******************
+'''
+import os #importing the os module
+#import flask #import tensorflow 
+#import tensorflow
+print("Hello, this is Mandeep, Citizen of India")
